78cfdfac9fa77a99e316c39570acc99f66175676
ivanmouradev <ivan.moura@ifpi.edu.br>
2025-11-05 19:14:18 -0300

    vingadores

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Nova mudança 
-// Comentário adicionado manualmente
+// Vigadores ultimato
 
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
