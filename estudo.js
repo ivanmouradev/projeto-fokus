94674ddb90602d59efaa6fe905c91c294eca3c6c
ivanmouradev <ivan.moura@ifpi.edu.br>
2025-11-05 15:04:35 -0300

    Update estudo.js
    mudança manual

--- estudo.js
+++ estudo.js
@@ -1,3 +1,4 @@
+// eu modifiquei isso :)
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
