47634a18fee4ff28430673355c54e39c989ed73a
Ivan Rodrigues <54-ivan@users.noreply.gitlab.lsdi.ufma.br>
2025-11-05 15:05:31 -0300

    comentario no estudo.js

--- estudo.js
+++ estudo.js
@@ -1,3 +1,4 @@
+//comentario local
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
